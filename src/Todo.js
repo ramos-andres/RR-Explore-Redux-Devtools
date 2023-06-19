7a0bb0893b19fa0767553e3dbb0f95e91cbccb94
ramos-andres <102202582+ramos-andres@users.noreply.github.com>
2023-06-18 22:21:43 -0700

    Added dispatch code

---
+++ src/Todo.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { addTodo, clearTodo } from './features/todoSlice'
+
+function Todo() {
+    const items = useSelector(state => state.todos.items)
+    const dispatch = useDispatch()
+    const [input, setInput] = useState('')
+
+    const renderItems = items.map((item, index) => <li key={index}>{item}</li>)
+
+    const submitForm = (e) => {
+        e.preventDefault()
+        dispatch(addTodo(input))
+    }
+
+    return (
+        <div>
+        <form onSubmit={(e) => submitForm(e)}>
+            <input value={input} onChange={(e) => setInput(e.target.value)} />   
+            <button type="submit">Submit</button> 
+        </form>
+        <ul>
+            {renderItems}
+        </ul>
+        <button onClick={() => dispatch(clearTodo())}>Clear</button>
+        </div>
+    )
+}
+
+export default Todo
+
