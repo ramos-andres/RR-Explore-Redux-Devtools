25099aa10b7514238ce15f39c10f4e929ff016d0
ramos-andres <102202582+ramos-andres@users.noreply.github.com>
2023-06-18 21:10:53 -0700

    Added todoSlice

---
+++ src/features/todoSlice.js
@@ -0,0 +1,25 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+    items: []
+};
+
+export const todoSlice = createSlice({
+    name: 'todos',
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            return { items: [...state.items, action.payload] }
+        },
+        removeTodo: (state, action) => {
+            return { items: state.items.filter(item => item.id !== action.payload.id) }
+        },
+        clearTodo: () => {
+            return { items: [] }
+        }
+    }
+});
+
+export const {addTodo, removeTodo, clearTodo} = todoSlice.actions;
+
+export default todoSlice.reducer;
